22d9fd3229adf34a49a6a09043d8d2d8aca7a255
rohit88-gif <rohit1416shinde@gmail.com>
2025-08-19 13:54:45 +0530

    Add files via upload

---
+++ evenOdd.c
@@ -0,0 +1,18 @@
+
+int main()
+{
+	int num;
+	
+	printf("enter the number");
+	scanf("%d",&num);
+	
+	if(num%2==0)
+	{
+		printf("it is a even number");
+	}
+	else
+	{
+		printf("it the odd  number");
+	}
+	
+}
